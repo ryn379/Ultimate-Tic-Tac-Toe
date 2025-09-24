dcea7ae162b675c41c04260e49019ec9cc06d52c
aryan <aryandwivedi120076@gmail.com>
2025-09-25 02:44:11 +0530

    Update docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/<Ultimate-Tic-Tac-Toe>/", // <-- add this
+  base: "/Ultimate-Tic-Tac-Toe/", // no angle brackets
 });
